2dad570cd46622e8a4bad52f9abd906f177ec8d7
Abdulk077 <Khanabdulwadud3@gmail.com>
2025-02-28 21:22:57 +0530

    Resolving isuue

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { signUp, signOut, signIn } from "../controllers/auth.controlller";
+import { signUp, signOut, signIn } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
 authRouter.post("/sign-up", signUp);
 authRouter.post("/sign-in", signIn);
 authRouter.post("/sign-out", signOut);
+
 
 export default authRouter;
